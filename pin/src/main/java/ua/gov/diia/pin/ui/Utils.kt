8601b6e15e5b6fba0bda4027aee34cc67753c761
omaeewa <pro22@macbook-pro-sergej.local>
2024-08-26 10:17:17 +0300

    Refactor: Remove duplicate code in PIN screen

---
+++ pin/src/main/java/ua/gov/diia/pin/ui/Utils.kt
@@ -0,0 +1,38 @@
+package ua.gov.diia.pin.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import ua.gov.diia.ui_base.R
+import ua.gov.diia.ui_base.components.DiiaResourceIcon
+import ua.gov.diia.ui_base.components.atom.text.textwithparameter.TextWithParametersData
+import ua.gov.diia.ui_base.components.infrastructure.DataActionWrapper
+import ua.gov.diia.ui_base.components.infrastructure.utils.resource.UiText
+import ua.gov.diia.ui_base.components.molecule.header.TitleGroupMlcData
+import ua.gov.diia.ui_base.components.organism.header.TopGroupOrgData
+import ua.gov.diia.ui_base.components.organism.tile.NumButtonTileOrganismData
+
+@Composable
+inline fun <reified T> List<T>.asSnapshotStateList() =
+    remember { mutableStateListOf(*this.toTypedArray()) }
+
+fun getPinTestData(topGroupText: String, textWithParameters: String, pinLength: Int = 4) = listOf(
+    TopGroupOrgData(
+        titleGroupMlcData = TitleGroupMlcData(
+            heroText = UiText.DynamicString(topGroupText),
+            leftNavIcon = TitleGroupMlcData.LeftNavIcon(
+                code = DiiaResourceIcon.BACK.code,
+                accessibilityDescription = UiText.StringResource(R.string.accessibility_back_button),
+                action = DataActionWrapper(
+                    type = "back",
+                    subtype = null,
+                    resource = null
+                )
+            )
+        )
+    ),
+    TextWithParametersData(
+        text = UiText.DynamicString(textWithParameters)
+    ),
+    NumButtonTileOrganismData(pinLength = pinLength)
+)
--- pin/src/main/java/ua/gov/diia/pin/ui/create/compose/CreatePinScreen.kt
+++ pin/src/main/java/ua/gov/diia/pin/ui/create/compose/CreatePinScreen.kt
@@ -3,8 +3,6 @@ package ua.gov.diia.pin.ui.create.compose
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
@@ -12,14 +10,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ConstraintLayout
+import ua.gov.diia.pin.ui.asSnapshotStateList
+import ua.gov.diia.pin.ui.getPinTestData
 import ua.gov.diia.ui_base.R
-import ua.gov.diia.ui_base.components.DiiaResourceIcon
-import ua.gov.diia.ui_base.components.atom.text.textwithparameter.TextWithParametersData
-import ua.gov.diia.ui_base.components.infrastructure.DataActionWrapper
 import ua.gov.diia.ui_base.components.infrastructure.UIElementData
 import ua.gov.diia.ui_base.components.infrastructure.event.UIAction
-import ua.gov.diia.ui_base.components.infrastructure.utils.resource.UiText
-import ua.gov.diia.ui_base.components.molecule.header.TitleGroupMlcData
 import ua.gov.diia.ui_base.components.molecule.text.TextLabelMlc
 import ua.gov.diia.ui_base.components.molecule.text.TextLabelMlcData
 import ua.gov.diia.ui_base.components.organism.header.TopGroupOrg
@@ -89,66 +84,24 @@ fun CreatePinScreen(
     }
 }
 
+private val testData = getPinTestData(
+    topGroupText = "Повторіть код з 4 цифр",
+    textWithParameters = "Цей код ви будете вводити для входу у застосунок Дія.",
+    pinLength = 5
+)
+
 @Composable
 @Preview
 fun CreatePinScreenPreview() {
-    val _uiData = remember { mutableStateListOf<UIElementData>() }
-    val uiData: SnapshotStateList<UIElementData> = _uiData
-    _uiData.add(
-        TopGroupOrgData(
-            titleGroupMlcData = TitleGroupMlcData(
-                heroText = UiText.DynamicString("Повторіть код з 4 цифр"),
-                leftNavIcon = TitleGroupMlcData.LeftNavIcon(
-                    code = DiiaResourceIcon.BACK.code,
-                    accessibilityDescription = UiText.StringResource(R.string.accessibility_back_button),
-                    action = DataActionWrapper(
-                        type = "back",
-                        subtype = null,
-                        resource = null
-                    )
-                )
-            )
-        )
-    )
-    _uiData.add(
-        TextLabelMlcData(
-            text = UiText.DynamicString("Цей код ви будете вводити для входу у застосунок Дія.")
-        )
-    )
-    _uiData.add(
-        NumButtonTileOrganismData()
-    )
+    val uiData = testData.asSnapshotStateList()
+
     CreatePinScreen(data = uiData, onUIAction = { })
 }
 
 @Composable
 @Preview(name = "phone", device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480")
 fun CreatePinScreenPreview_small_screen() {
-    val _uiData = remember { mutableStateListOf<UIElementData>() }
-    val uiData: SnapshotStateList<UIElementData> = _uiData
-    _uiData.add(
-        TopGroupOrgData(
-            titleGroupMlcData = TitleGroupMlcData(
-                heroText = UiText.DynamicString("Повторіть код з 4 цифр"),
-                leftNavIcon = TitleGroupMlcData.LeftNavIcon(
-                    code = DiiaResourceIcon.BACK.code,
-                    accessibilityDescription = UiText.StringResource(R.string.accessibility_back_button),
-                    action = DataActionWrapper(
-                        type = "back",
-                        subtype = null,
-                        resource = null
-                    )
-                )
-            )
-        )
-    )
-    _uiData.add(
-        TextWithParametersData(
-            text = UiText.DynamicString("Цей код ви будете вводити для входу у застосунок Дія.")
-        )
-    )
-    _uiData.add(
-        NumButtonTileOrganismData(pinLength = 5)
-    )
+    val uiData = testData.asSnapshotStateList()
+
     CreatePinScreen(data = uiData, onUIAction = { })
 }
